3cdd28ea6b562e5c29f296467fbba9207ea25ca3
is2js <tingstyle1@gmail.com>
2022-07-18 00:04:05 +0900

    feat(utils): 컨텍스트관리자를 이용한 시간측정 데코레이터 클래스 구현

---
+++ utils/Timer.py
@@ -0,0 +1,18 @@
+import time
+import contextlib
+
+
+# 1. 데코레이터로 class를 생성하되, contextlib.ContextDecorator를 상속해서 만든다.
+class Timer(contextlib.ContextDecorator):
+
+    def __init__(self, name) -> None:
+        self.name = name
+
+    # 2. enter와 exit를 구현하며, 시작 데코로직을 enter에 / 종료 데코로직을 exit에 구현한다.
+    def __enter__(self):
+        self.start = time.time()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.end = time.time()
+        operation_time = self.end - self.start
+        print(f">>[Timer] 수행시간 of {self.name}: {operation_time} sec")
